399caa4ebfbb27c9b1b08b69bc5588740fd3cde7
PeterFiorentino <peterfiorentino@pursuit.org>
2020-11-28 15:30:49 -0500

    made comments on the code directly as well as a .txt file to break down the grading rubric

--- Board.js
+++ Board.js
@@ -1,6 +1,8 @@
 class Board {
   constructor(wordLength){
     this.board = new Array(wordLength).fill("_")
+    //I like this way of approaching making an array of equal length but filled with "_"s
+    //A lot cleaner than looping through the length of magicWord and adding "_" to a new array for every instance
   }
   isBoardComplete(){
     let complete = false;
@@ -9,7 +11,11 @@ class Board {
         complete = true;
       }
     }
+
     return complete
+    //The way this check is set up it will only return true/false based on whether or not the last letter (this.board[this.board.length -1]) === "_"
+    //Remember, a completed word is on that does not include any "_"s
+    //The logic to solving this is similar to your isCorrectGuess function in snowman.js
   }
   displayBoard(){
     console.log(this.board)
--- snowman.js
+++ snowman.js
@@ -6,14 +6,19 @@ const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split(""
 class Game {
   constructor(){
     this.name = readline.question("Hello! Please enter your name: ")
+    //I like this addition! Makes the game more interactive for the user and fun to see their name at the end when they win.
     this.magicWord = dictionary[Math.floor(Math.random() * dictionary.length)]
     this.wrongGuessesRemaining = 6
     this.guesses = []
+    //Try grouping wrongGuessesRemaining and guesses into an object!
     this.board = new Board(this.magicWord.length)
   }
 
   getMove() {
     return readline.question("Guess any letter:")
+    //Your readline question implimentation is correct! 
+    //I'd just like to see the magicWord (blocked with "_"s) and amount of guesses left before the user enters their first guess.
+    //See .gif in the assignment's github repo https://github.com/joinpursuit/FSW-CLI-Snowman
   }
 
   isCorrectGuess(letter) {
@@ -23,14 +28,19 @@ class Game {
       return false
     }
   }
+  //Perfect use of .includes to check if a user's input is part of magicWord!
 
   isValidGuess(letter){
     if (alphabet.includes(letter) && this.guesses.includes(letter)){
+      //"this.guesses.includes(letter)" makes it so we only consider letters the user guessed already as valid options, instead of letters in the alphabet.
+      //This causes your user to enter a letter twice in order to have it as a valid option in the game which confuses game play. 
+      //Consider what makes an input a valid guess and construct your condition accordingly.
       return true
     } else {
       return false
     }
   }
+
   isGameOver(){
     if (this.wrongGuessesRemaining <= 0 || this.board.isBoardComplete()){
       return true 
@@ -46,10 +56,17 @@ class Game {
       }
     }
   }
+
+  //Both isGameOver() and placeLetter() are exactly what we're looking for in these functions! Good job. 
+
   play(){
     while (!this.isGameOver()){
       let letter = this.getMove()
-      while (!this.isValidGuess(letter)) {
+      //I played a few rounds after fixing isValidGuess() and noticed that capitalized letters are considered valid guesses but are always wrong, even when they are included in magicWord.
+      //This is because magicWord is getting words from a dictionary of entirely lower cased words.
+      //Look at this resource for a solution https://www.w3schools.com/jsref/jsref_tolowercase.asp
+      //Slack me if you have trouble implimenting it!
+      while(!this.isValidGuess(letter)) {
         console.log("Sorry, that is not a valid guess.");
         this.wrongGuessesRemaining--
         letter = this.getMove()
@@ -57,14 +74,18 @@ class Game {
       if (this.isCorrectGuess(letter)) {
         this.placeLetter(letter)
         this.wrongGuessesRemaining--;
+        // This line above will also subtract from a users guesses even when they guess correctly.
+        // It is confusing for gameplay and also makes words over 6 unique letters impossible to correctly guess.
       } else {
         console.log("Incorrect!")
         this.wrongGuessesRemaining--;
       }
-      // console.log(this.magicWord)
+    
       this.board.displayBoard()
       console.log("You have " + this.wrongGuessesRemaining + " guesses left")
+      //I'd also love for a user to be able to see the letters they have already guessed (both correct and incorrect) to prevent them from guessing the same letter twice.
     }
+
   }
 
 }
@@ -78,5 +99,8 @@ else if (game.isGameOver() && game.wrongGuessesRemaining <= 0){
   console.log("Sorry, you lost. The word was " + game.magicWord);
   
 }
-
+//This if and else if will never hit here. 
+//If they both require game.isGameOver() to be true then let's try move them to the part of play() where isGameOver() is true!
+//Also, as the user, I'd love to see how guesses it took me to complete the game if I won
+//If I lose, I'd like to see the word revealed to me!
 game.play()
